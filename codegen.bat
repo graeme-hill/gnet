09738e91a9a788911291a2ab502715ac14d3a907
Graeme Hill <graemekh@gmail.com>
2019-05-17 20:37:49 -0700

    windows codegen script

---
+++ codegen.bat
@@ -0,0 +1,5 @@
+protoc --go_out=. chat/pb/protos.proto
+protoc --go_out=plugins=grpc:. sys/rpc-domainevents/pb/protos.proto
+protoc --go_out=plugins=grpc:. sys/scan/pb/protos.proto
+protoc --go_out=. iam/pb/protos.proto
+protoc --go_out=. photos/events/pb/photos.proto
